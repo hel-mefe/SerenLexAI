5d657c185106ee07e56be78b25b48ec4e23faba8
hel-mefe <elmefeddelhicham@gmail.com>
2026-03-03 11:40:18 +0000

    style: delete dashboard header for UI preferences

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -16,7 +16,7 @@ export function DashboardPage() {
       <Sidebar />
 
       <div className="flex-1 flex flex-col min-w-0">
-        <DashboardHeader />
+        {/* <DashboardHeader /> */}
 
         <main className="flex-1 p-8 overflow-auto space-y-8">
           {/* Action Cards */}
